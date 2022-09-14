8a1fc153f750a49aa8156d786fd1a68c53d9f26d
lazovicff <lazovicff@gmail.com>
2022-09-14 13:58:05 +0200

    Change update logic

--- protocol/src/peer/mod.rs
+++ protocol/src/peer/mod.rs
@@ -157,15 +157,11 @@ impl Peer {
 		let pubkey_p = self.get_pub_key(key.0).ok_or(EigenError::PubkeyNotFound)?;
 		// We add it only if its a valid proof
 		let res = opinion.verify(&pubkey_p, &self.keypair, &self.params, vk)?;
-		match res {
-			true => {
-				self.cached_neighbor_opinion.insert(key, opinion);
-				log::debug!("Neighbour opinion is added {:?}", key);
-			},
-			false => {
-				log::debug!("Neighbour opinion is not valid {:?}", key);
-			},
-		};
+		if res {
+			self.cached_neighbor_opinion.insert(key, opinion);
+		} else {
+			log::debug!("Neighbour opinion is not valid {:?}", key);
+		}
 		Ok(())
 	}
 
