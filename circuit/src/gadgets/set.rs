11c639cdc5682478b873df7ed3283f4ed12ab0ac
4rmanc <ozgurarmancyigit@hotmail.com>
2022-09-22 02:42:18 +0300

    documentation

--- circuit/src/gadgets/set.rs
+++ circuit/src/gadgets/set.rs
@@ -7,21 +7,32 @@ use halo2wrong::halo2::{
 };
 
 #[derive(Clone, Debug)]
+/// Configuration elements for the circuit defined here.
 pub struct FixedSetConfig {
+	/// Constructs is_zero circuit elements.
 	is_zero: IsZeroConfig,
+	/// Configures a column for the target.
 	target: Column<Advice>,
+	/// Configures a fixed column for the items.
 	items: Column<Fixed>,
+	/// Configures a column for the diffs.
 	diffs: Column<Advice>,
+	/// Configures a column for the product.
 	product: Column<Advice>,
+	/// Configures a fixed boolean value for each row of the circuit.
 	selector: Selector,
 }
 
+/// Constructs individual cells for the configuration elements.
 pub struct FixedSetChip<F: FieldExt, const N: usize> {
+	/// Constructs items variable for the circuit.
 	items: [F; N],
+	/// Assigns a cell for the target.
 	target: AssignedCell<F, F>,
 }
 
 impl<F: FieldExt, const N: usize> FixedSetChip<F, N> {
+	/// Create a new chip.
 	pub fn new(items: [F; N], target: AssignedCell<F, F>) -> Self {
 		FixedSetChip { items, target }
 	}
@@ -53,15 +64,24 @@ impl<F: FieldExt, const N: usize> FixedSetChip<F, N> {
 			let s_exp = v_cells.query_selector(s);
 
 			vec![
+				// If difference == 0, that will make products equal to 0 in the circuit.
+				// Example:
+				// product_exp = 1
+				// diff_exp = 0
+				// Check the constraint 1 * 0 == next_product_exp
+				// That makes next_product_exp = 0
+				// => (1 * 0) - 0 == 0
 				s_exp.clone() * (product_exp * diff_exp.clone() - next_product_exp),
-				// TODO: uncomment this line when the bug is fixed.
-				// s_exp * (target_exp.clone() - (diff_exp + item_exp)),
+				// diff_exp = target_exp - item_exp
+				// If diff_exp == 0, target_exp - item_exp will be equal to 0 as well.
+				s_exp * (target_exp - (diff_exp + item_exp)),
 			]
 		});
 
 		FixedSetConfig { is_zero, target, items, diffs, product, selector: s }
 	}
 
+	/// Synthesize the circuit.
 	pub fn synthesize(
 		&self, config: FixedSetConfig, mut layouter: impl Layouter<F>,
 	) -> Result<AssignedCell<F, F>, Error> {
@@ -87,7 +107,10 @@ impl<F: FieldExt, const N: usize> FixedSetChip<F, N> {
 						|| item_value,
 					)?;
 
+					// Calculating difference between given target and item from the set.
 					let diff = self.target.value().cloned() - item_value;
+					// If difference is 0, that means target is in the set and next product will
+					// become 0.
 					let next_product = assigned_product.value().cloned() * diff;
 
 					region.assign_advice(|| format!("diff_{}", i), config.diffs, i, || diff)?;
@@ -124,6 +147,7 @@ mod test {
 	use halo2wrong::{
 		curves::bn256::{Bn256, Fr},
 		halo2::{
+			arithmetic::Field,
 			circuit::{SimpleFloorPlanner, Value},
 			dev::MockProver,
 			plonk::{Circuit, Instance},
@@ -138,18 +162,18 @@ mod test {
 	}
 
 	#[derive(Clone)]
-	struct TestCircuit<F: FieldExt> {
-		items: [F; 3],
+	struct TestCircuit<F: FieldExt, const N: usize> {
+		items: [F; N],
 		target: Value<F>,
 	}
 
-	impl<F: FieldExt> TestCircuit<F> {
-		fn new(items: [F; 3], target: F) -> Self {
+	impl<F: FieldExt, const N: usize> TestCircuit<F, N> {
+		fn new(items: [F; N], target: F) -> Self {
 			Self { items, target: Value::known(target) }
 		}
 	}
 
-	impl<F: FieldExt> Circuit<F> for TestCircuit<F> {
+	impl<F: FieldExt, const N: usize> Circuit<F> for TestCircuit<F, N> {
 		type Config = TestConfig;
 		type FloorPlanner = SimpleFloorPlanner;
 
@@ -187,12 +211,52 @@ mod test {
 
 	#[test]
 	fn test_is_member() {
+		// Testing a target value from the set.
 		let set = [Fr::from(1), Fr::from(2), Fr::from(3)];
 		let target = Fr::from(2);
 		let test_chip = TestCircuit::new(set, target);
 
 		let pub_ins = vec![Fr::one()];
 		let k = 4;
+		let prover = MockProver::run(k, &test_chip, vec![pub_ins]).unwrap();
+		assert_eq!(prover.verify(), Ok(()));
+	}
+
+	#[test]
+	fn test_is_not_member() {
+		// Testing a target value that is not in the set.
+		let set = [Fr::from(1), Fr::from(2), Fr::from(4), Fr::from(15), Fr::from(23)];
+		let target = Fr::from(12);
+		let test_chip = TestCircuit::new(set, target);
+
+		let pub_ins = vec![Fr::zero()];
+		let k = 4;
+		let prover = MockProver::run(k, &test_chip, vec![pub_ins]).unwrap();
+		assert_eq!(prover.verify(), Ok(()));
+	}
+
+	#[test]
+	fn test_is_member_zero() {
+		// Testing target value as 0.
+		let set = [Fr::from(0), Fr::from(4), Fr::from(5), Fr::from(11), Fr::from(13)];
+		let target = Fr::from(0);
+		let test_chip = TestCircuit::new(set, target);
+
+		let pub_ins = vec![Fr::one()];
+		let k = 4;
+		let prover = MockProver::run(k, &test_chip, vec![pub_ins]).unwrap();
+		assert_eq!(prover.verify(), Ok(()));
+	}
+
+	#[test]
+	fn test_is_member_big() {
+		// Testing a big set.
+		let set = [(); 1024].map(|_| <Fr as Field>::random(rand::thread_rng()));
+		let target = set[722].clone();
+		let test_chip = TestCircuit::new(set, target);
+
+		let pub_ins = vec![Fr::one()];
+		let k = 11;
 		let prover = MockProver::run(k, &test_chip, vec![pub_ins]).unwrap();
 		assert_eq!(prover.verify(), Ok(()));
 	}
