b3fa5ffeddbdaa72e298096b7390f1c18c21630d
4rmanc <ozgurarmancyigit@hotmail.com>
2022-11-07 17:39:34 +0300

    documentation

--- circuit/src/wrong/mod.rs
+++ circuit/src/wrong/mod.rs
@@ -1,2 +1,4 @@
+/// Native implementation for the non-native field arithmetic
 pub mod native;
+/// RNS operations for the non-native field arithmetic
 pub mod rns;
--- circuit/src/wrong/rns.rs
+++ circuit/src/wrong/rns.rs
@@ -1,4 +1,3 @@
-#![allow(missing_docs)]
 // Rns
 // bit_len_lookup: 17,
 // wrong_modulus:
@@ -61,49 +60,55 @@
 // mul_v_bit_len: 71,
 // red_v_bit_len: 69,
 
-use std::{result, str::FromStr};
-
 use halo2wrong::{
-	curves::{
-		bn256::{Fq, Fr},
-		group::ff::PrimeField,
-	},
+	curves::bn256::{Fq, Fr},
 	halo2::arithmetic::{Field, FieldExt},
 };
 use num_bigint::BigUint;
 use num_integer::Integer;
 use num_traits::{FromPrimitive, Num, Zero};
-use std::ops::Shl;
+use std::{ops::Shl, str::FromStr};
 
+/// This trait is for the dealing with RNS operations.
 pub trait RnsParams<W: FieldExt, N: FieldExt, const NUM_LIMBS: usize, const NUM_BITS: usize> {
+	/// Returns Scalar (Native) Field modulus [`Fr`] from Bn256.
 	fn native_modulus() -> BigUint;
+	/// Returns Base (Wrong) Field modulus [`Fq`] from Bn256.
 	fn wrong_modulus() -> BigUint;
+	/// Returns negative Base (Wrong) Field as decomposed.
 	fn negative_wrong_modulus_decomposed() -> [N; NUM_LIMBS];
+	/// Returns right shifters.
 	fn right_shifters() -> [N; NUM_LIMBS];
+	/// Returns left shifters.
 	fn left_shifters() -> [N; NUM_LIMBS];
-	fn max_reduced_limb() -> N;
-	fn max_unreduced_limb() -> N;
-	fn max_remainder() -> BigUint;
+	/// Returns residue value from given inputs.
 	fn residues(n: &[N; NUM_LIMBS], t: &[N; NUM_LIMBS]) -> Vec<N>;
+	/// Returns `quotient` and `remainder` for the mul operation.
 	fn construct_mul_qr(a_bn: BigUint, b_bn: BigUint) -> ([N; NUM_LIMBS], [N; NUM_LIMBS]);
+	/// Returns `quotient` and `remainder` for the add operation.
 	fn construct_add_qr(a_bn: BigUint, b_bn: BigUint) -> (N, [N; NUM_LIMBS]);
+	/// Constraint for the binary part of `Chinese Remainder Theorem`.
 	fn constrain_binary_crt(t: [N; 4], result: [N; 4], residues: Vec<N>) -> bool;
 }
 
+/// Returns modulus of the [`FieldExt`] as [`BigUint`].
 pub fn modulus<F: FieldExt>() -> BigUint {
 	BigUint::from_str_radix(&F::MODULUS[2..], 16).unwrap()
 }
 
+/// Returns [`FieldExt`] for the given [`BigUint`].
 pub fn big_to_fe<F: FieldExt>(e: BigUint) -> F {
 	let modulus = modulus::<F>();
 	let e = e % modulus;
 	F::from_str_vartime(&e.to_str_radix(10)[..]).unwrap()
 }
 
+/// Returns [`BigUint`] representation for the given [`FieldExt`].
 pub fn fe_to_big<F: FieldExt>(fe: F) -> BigUint {
 	BigUint::from_bytes_le(fe.to_repr().as_ref())
 }
 
+/// Returns `limbs` by decomposing [`BigUint`].
 pub fn decompose_big<F: FieldExt, const NUM_LIMBS: usize, const BIT_LEN: usize>(
 	mut e: BigUint,
 ) -> [F; NUM_LIMBS] {
@@ -112,13 +117,12 @@ pub fn decompose_big<F: FieldExt, const NUM_LIMBS: usize, const BIT_LEN: usize>(
 	for i in 0..NUM_LIMBS {
 		let limb = mask.clone() & e.clone();
 		e = e.clone() >> BIT_LEN;
-		//println!("{}", limb.to_str_radix(16));
 		limbs[i] = big_to_fe(limb);
 	}
-
 	limbs
 }
 
+/// Returns [`BigUint`] by composing `limbs`.
 pub fn compose<const NUM_LIMBS: usize, const NUM_BITS: usize>(
 	input: [BigUint; NUM_LIMBS],
 ) -> BigUint {
@@ -129,20 +133,7 @@ pub fn compose<const NUM_LIMBS: usize, const NUM_BITS: usize>(
 	res
 }
 
-pub fn terms_compose(input: Vec<Fr>, quotient: Fr) -> Fr{
-	let input: Vec<Fr> = input.iter().filter(|e| !bool::from(e.is_zero())).cloned().collect();
-	let mut result = Fr::zero();
-	for i in 0..input.len(){
-		if quotient == Fr::zero() {
-			result = result + input[i];
-		}
-		else {
-		result = result + (input[i] * quotient);
-		}		
-	}
-	result
-}
-
+/// Structure for the Bn256_4_68
 #[derive(Debug, Clone)]
 pub struct Bn256_4_68;
 
@@ -191,21 +182,6 @@ impl RnsParams<Fq, Fr, 4, 68> for Bn256_4_68 {
 		[limb0, limb1, limb2, limb3]
 	}
 
-	fn max_reduced_limb() -> Fr {
-		Fr::from_u128(295147905179352825855)
-	}
-
-	fn max_unreduced_limb() -> Fr {
-		Fr::from_u128(5070602400912917605986812821503)
-	}
-
-	fn max_remainder() -> BigUint {
-		BigUint::from_str(
-			"28948022309329048855892746252171976963317496166410141009864396001978282409983",
-		)
-		.unwrap()
-	}
-
 	fn residues(n: &[Fr; 4], t: &[Fr; 4]) -> Vec<Fr> {
 		let lsh1 = Self::left_shifters()[1];
 		let rsh2 = Self::right_shifters()[2];
@@ -234,13 +210,14 @@ impl RnsParams<Fq, Fr, 4, 68> for Bn256_4_68 {
 	fn construct_add_qr(a_bn: BigUint, b_bn: BigUint) -> (Fr, [Fr; 4]) {
 		let wrong_mod_bn = Self::wrong_modulus();
 		let (quotient, result_bn) = (a_bn + b_bn).div_rem(&wrong_mod_bn);
+		// This check assures that the addition operation can only wrap the wrong field
+		// one time.
 		assert!(quotient <= BigUint::from_u8(1).unwrap());
 		let q = big_to_fe(quotient);
 		let result = decompose_big::<Fr, 4, 68>(result_bn);
 		(q, result)
 	}
 
-	// USED FOR TESTING PURPOSES
 	fn constrain_binary_crt(t: [Fr; 4], result: [Fr; 4], residues: Vec<Fr>) -> bool {
 		let lsh_one = Self::left_shifters()[1];
 		let lsh_two = Self::left_shifters()[2];
@@ -250,241 +227,12 @@ impl RnsParams<Fq, Fr, 4, 68> for Bn256_4_68 {
 		for i in (0..4).step_by(2) {
 			let (t_lo, t_hi) = (t[i], t[i + 1]);
 			let (r_lo, r_hi) = (result[i], result[i + 1]);
-
 			// CONSTRAINT
 			let res = t_lo + t_hi * lsh_one - r_lo - r_hi * lsh_one - residues[i / 2] * lsh_two + v;
 			v = residues[i / 2];
 			let res_is_zero: bool = res.is_zero().into();
 			is_satisfied = is_satisfied | res_is_zero;
 		}
-
 		is_satisfied
-	}
-}
-
-#[cfg(test)]
-mod test {
-	use super::*;
-
-	fn residues(n: &[Fr; 4], t: &[Fr; 4]) -> Vec<Fr> {
-		let lsh1 = Bn256_4_68::left_shifters()[1];
-		let rsh2 = Bn256_4_68::right_shifters()[2];
-
-		let mut res = Vec::new();
-		let mut carry = Fr::zero();
-		for i in (0..4).step_by(2) {
-			let (t_0, t_1) = (t[i], t[i + 1]);
-			let (r_0, r_1) = (n[i], n[i + 1]);
-			let u = t_0 + (t_1 * lsh1) - r_0 - (lsh1 * r_1) + carry;
-			let v = u * rsh2;
-			carry = v;
-			res.push(v)
-		}
-		res
-	}
-
-	fn constrain_residues(t: [Fr; 4], result: [Fr; 4], residues: Vec<Fr>) {
-		let lsh_one = Bn256_4_68::left_shifters()[1];
-		let lsh_two = Bn256_4_68::left_shifters()[2];
-
-		let mut v = Fr::zero();
-		for i in (0..4).step_by(2) {
-			let (t_lo, t_hi) = (t[i], t[i + 1]);
-			let (r_lo, r_hi) = (result[i], result[i + 1]);
-
-			// CONSTRAINT
-			let res = t_lo + t_hi * lsh_one - r_lo - r_hi * lsh_one - residues[i / 2] * lsh_two + v;
-			v = residues[i / 2];
-			//println!("Binary part constraint {:?}", res);
-		}
-	}
-
-	#[test]
-	pub fn wrong_mul() {
-		let native_mod_bn = Bn256_4_68::native_modulus();
-		let wrong_mod_bn = Bn256_4_68::wrong_modulus();
-		let p_prime = Bn256_4_68::negative_wrong_modulus_decomposed();
-
-		let a_bn = BigUint::from_str(
-			"91888242871839275222246405745257275088548364400416034343698204186575808495807",
-		)
-		.unwrap();
-		let b_bn = BigUint::from_u64(123134).unwrap();
-		let (quotient, result_bn) = (a_bn.clone() * b_bn.clone()).div_rem(&wrong_mod_bn);
-
-		let a = decompose_big::<Fr, 4, 68>(a_bn.clone());
-		let b = decompose_big::<Fr, 4, 68>(b_bn.clone());
-		let q = decompose_big::<Fr, 4, 68>(quotient.clone());
-		let result = decompose_big::<Fr, 4, 68>(result_bn.clone());
-		let p = decompose_big::<Fr, 4, 68>(wrong_mod_bn.clone());
-
-		let mut t: [Fr; 4] = [Fr::zero(); 4];
-
-		for k in 0..4 {
-			for i in 0..=k {
-				let j = k - i;
-				t[i + j] = t[i + j] + a[i] * b[j] + p_prime[i] * q[j];
-			}
-		}
-
-		let residues = residues(&result, &t);
-
-		// a = 8
-		// b = 3
-		// p = 13
-		// n = 5
-
-		// a_native = a % 5
-		// b_native = b % 5
-		// p_native = p % 5
-
-		let mut new_t: Vec<Fr> = vec![];
-		for (i, inter) in t.iter().enumerate() {
-			let mut inter = *inter;
-			for j in 0..=i {
-				let k = i - j;
-
-				let prev_inter = inter;
-				let next_inter = inter - (a[j] * b[k] + q[k] * p_prime[j]);
-
-				if j == 0 {
-					new_t.push(prev_inter);
-				}
-				// CONSTRAINT
-				println!(
-					"{:?}",
-					a[j] * b[k] + q[k] * p_prime[j] - prev_inter + next_inter
-				);
-
-				inter = next_inter;
-			}
-		}
-
-		constrain_residues(new_t.try_into().unwrap(), result, residues);
-
-		let a_native = Fr::from_str_vartime(
-			"91888242871839275222246405745257275088548364400416034343698204186575808495807",
-		)
-		.unwrap();
-		let b_native = Fr::from_str_vartime("123134").unwrap();
-		let wrong_mod_native = Fr::from_str_vartime(
-			"21888242871839275222246405745257275088696311157297823662689037894645226208583",
-		)
-		.unwrap();
-		let res_native = Fr::from_str_vartime(
-			"8839498411810231587881575137642804062353405807773865066449658236690677140446",
-		)
-		.unwrap();
-		let q_native = Fr::from_str_vartime("516924").unwrap();
-
-		// CONSTRAINT
-		let resa = a_native * b_native - q_native * wrong_mod_native - res_native;
-		//println!("native {:?}", resa);
-	}
-
-	#[test]
-	fn reduce() {
-		let native_mod_bn = Bn256_4_68::native_modulus();
-		let wrong_mod_bn = Bn256_4_68::wrong_modulus();
-		let p_prime = Bn256_4_68::negative_wrong_modulus_decomposed();
-
-		let val = BigUint::from_str(
-			"91888242871839275222246405745257275088548364400416034343698204186575808495807",
-		)
-		.unwrap();
-		let (quotient, result_bn) = val.div_rem(&wrong_mod_bn);
-		assert!(quotient < BigUint::from_u8(1).unwrap() << 68u8.into());
-
-		let q_native = big_to_fe::<Fr>(quotient);
-		let result_limbs = decompose_big::<Fr, 4, 68>(result_bn.clone());
-		let val_limbs = decompose_big::<Fr, 4, 68>(val);
-
-		let mut t = [Fr::zero(); 4];
-		for i in 0..4 {
-			let res = val_limbs[i] + p_prime[i] * q_native;
-			t[i] = res;
-		}
-
-		let residues = residues(&result_limbs, &t);
-
-		// Binary Constraint
-		constrain_residues(t.try_into().unwrap(), result_limbs, residues);
-
-		let val_native = Fr::from_str_vartime(
-			"91888242871839275222246405745257275088548364400416034343698204186575808495807",
-		)
-		.unwrap();
-		let quotient_native = Fr::from_str_vartime("4").unwrap();
-		let wrong_mod_native = Fr::from_str_vartime(
-			"21888242871839275222246405745257275088696311157297823662689037894645226208583",
-		)
-		.unwrap();
-		let result_native = big_to_fe::<Fr>(result_bn.clone());
-		// Native Constraint
-		let reduce_result = quotient_native * wrong_mod_native - val_native + result_native;
-		//println!("reduced result = {:?}", reduce_result);
-	}
-
-	fn reduce_if_limb_values_exceeds() {
-		let num = (BigUint::from_u8(1).unwrap() << 68) - BigUint::from_u8(1).unwrap();
-		let max_unreduced = Bn256_4_68::max_unreduced_limb();
-		let max_reduced = Bn256_4_68::max_reduced_limb();
-
-		// ADD - If max limb value exceeds max_unreduced_limb
-		// MUL - If max limb value exceeds max_reduced_limb
-	}
-
-	#[test]
-	fn wrong_add() {
-		let max_unreduced = Bn256_4_68::max_unreduced_limb();
-		//println!("{:?}", max_unreduced);
-		let a_bn = BigUint::from_str("7961293874321").unwrap();
-		let b_bn = BigUint::from_str("187419823").unwrap();
-
-		let a = decompose_big::<Fr, 4, 68>(a_bn.clone());
-		let b = decompose_big::<Fr, 4, 68>(b_bn.clone());
-
-		let mut res: [Fr; 4] = [Fr::zero(); 4];
-		for i in 0..4 {
-			res[i] = a[i] + b[i];
-			//println!("a {}", fe_to_big(a[i]));
-			//println!("b {}", fe_to_big(b[i]));
-			//println!("res {:#?}", fe_to_big(res[i]));
-		}
-	}
-
-	#[test]
-	fn prepare_nums() {
-		let limb1 = Fr::from_str_vartime("295147905179352825856").unwrap();
-		let limb2 = Fr::from_str_vartime("87112285931760246646623899502532662132736").unwrap();
-		let limb3 =
-			Fr::from_str_vartime("25711008708143844408671393477458601640355247900524685364822016")
-				.unwrap();
-		let limb1_bn = BigUint::from_str("295147905179352825856").unwrap();
-		let limb2_bn = BigUint::from_str("87112285931760246646623899502532662132736").unwrap();
-		let limb3_bn =
-			BigUint::from_str("25711008708143844408671393477458601640355247900524685364822016")
-				.unwrap();
-		// decompose_big::<Fr, 4, 64>(limb1_bn);
-		// decompose_big::<Fr, 4, 64>(limb2_bn);
-		// decompose_big::<Fr, 4, 64>(limb3_bn);
-		// let limb1_raw = Fr::from_raw([0x0, 0x10, 0x0, 0x0]);
-		// let limb2_raw = Fr::from_raw([0x0, 0x0, 0x100, 0x0]);
-		// let limb3_raw = Fr::from_raw([0x0, 0x0, 0x0, 0x1000]);
-		// assert_eq!(limb1, limb1_raw);
-		// assert_eq!(limb2, limb2_raw);
-		// assert_eq!(limb3, limb3_raw);
-
-		// let fr_mod = BigUint::from_str_radix(
-		// 	"30644e72e131a029b85045b68181585d2833e84879b9709143e1f593f0000001",
-		// 	16,
-		// )
-		// .unwrap();
-		// let fq_mod = BigUint::from_str_radix(
-		// 	"30644e72e131a029b85045b68181585d97816a916871ca8d3c208c16d87cfd47",
-		// 	16,
-		// )
-		// .unwrap();
-		// println!("{} {} {}", fq_mod.bits(), fr_mod.bits(), fq_mod - fr_mod);
 	}
 }
