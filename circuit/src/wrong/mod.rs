6b2b91af9dc176d66daaf18e771e60d81ca73bc2
lazovicff <lazovicff@gmail.com>
2022-11-08 13:55:03 +0100

    WIP 2

--- circuit/src/wrong/mod.rs
+++ circuit/src/wrong/mod.rs
@@ -9,6 +9,7 @@ use halo2wrong::halo2::{
 	plonk::{Advice, Column, ConstraintSystem, Error, Expression, Fixed, Selector, VirtualCells},
 	poly::Rotation,
 };
+use native::ReductionWitness;
 use rns::RnsParams;
 use std::marker::PhantomData;
 
@@ -26,11 +27,7 @@ struct IntegerChip<W: FieldExt, N: FieldExt, const NUM_LIMBS: usize, const NUM_B
 where
 	P: RnsParams<W, N, NUM_LIMBS, NUM_BITS>,
 {
-	x_limbs: [AssignedCell<N, N>; NUM_LIMBS],
-	y_limbs: [AssignedCell<N, N>; NUM_LIMBS],
-	quotient: [AssignedCell<N, N>; NUM_LIMBS],
-	intermediate: [AssignedCell<N, N>; NUM_LIMBS],
-	residues: Vec<AssignedCell<N, N>>,
+	_native: PhantomData<N>,
 	_wrong: PhantomData<W>,
 	_rns: PhantomData<P>,
 }
@@ -50,67 +47,66 @@ where
 		let add_selector = meta.selector();
 		let mul_selector = meta.selector();
 
-		let lsh_one = P::left_shifters()[1];
-		let lsh_two = P::left_shifters()[2];
 		let p_prime = P::negative_wrong_modulus_decomposed();
-
-		meta.create_gate("reduction", |v_cells| {
-			let limb_exps = x_limbs.map(|x| v_cells.query_advice(x, Rotation::cur()));
-			let quotient_exps = quotient.map(|x| v_cells.query_advice(x, Rotation::cur()));
-			let intermediate_exps = intermediate.map(|x| v_cells.query_advice(x, Rotation::cur()));
-			let residues_exps: Vec<Expression<N>> =
-				residues.iter().map(|x| v_cells.query_advice(x, Rotation::cur())).collect();
-
-			let mut v = Expression::Constant(N::zero());
-			let mut constraints = Vec::new();
-			for i in (0..NUM_LIMBS).step_by(2) {
-				let (t_lo, t_hi) = (intermediate_exps[i], intermediate_exps[i + 1]);
-				let (r_lo, r_hi) = (limb_exps[i], limb_exps[i + 1]);
-				let res =
-					t_lo + t_hi * lsh_one - r_lo - r_hi * lsh_one - residues_exps[i / 2] * lsh_two
-						+ v;
-				v = residues[i / 2];
-				constraints.push(res);
-			}
-
-			constraints
-		});
+		let p_in_n = P::wrong_modulus_in_native_modulus();
 
 		meta.create_gate("add", |v_cells| {
+			let add_s = v_cells.query_selector(add_selector);
 			let x_limb_exps = x_limbs.map(|x| v_cells.query_advice(x, Rotation::cur()));
 			let y_limb_exps = y_limbs.map(|y| v_cells.query_advice(y, Rotation::cur()));
 			let add_q_exp = v_cells.query_advice(quotient[0], Rotation::cur());
+			// let t_exp = intermediate.map(|x| v_cells.query_advice(x, Rotation::c))
 			let result_exps = x_limbs.map(|x| v_cells.query_advice(x, Rotation::next()));
 			let residues_exps: Vec<Expression<N>> =
-				residues.iter().map(|x| v_cells.query_advice(x, Rotation::cur())).collect();
+				residues.iter().map(|x| v_cells.query_advice(*x, Rotation::cur())).collect();
 
-			let mut t = [Expression::Constant(N::zero()); NUM_LIMBS];
+			let mut t = [(); NUM_LIMBS].map(|_| Expression::Constant(N::zero()));
 			for i in 0..NUM_LIMBS {
-				t[i] = x_limb_exps[i] + y_limb_exps[i] + p_prime[i] * add_q_exp;
+				t[i] = x_limb_exps[i].clone()
+					+ y_limb_exps[i].clone()
+					+ add_q_exp.clone() * p_prime[i];
 			}
 
 			// REDUCTION CONSTRAINTS
-			P::constrain_binary_crt_exp(t, result_exps, residues_exps);
+			let mut constraints =
+				P::constrain_binary_crt_exp(t, result_exps.clone(), residues_exps);
+
+			let native_constraint = P::compose_exp(x_limb_exps) + P::compose_exp(y_limb_exps)
+				- P::compose_exp(result_exps);
+			constraints.push(native_constraint);
+
+			constraints.iter().map(|x| add_s.clone() * x.clone()).collect::<Vec<Expression<N>>>()
 		});
 
 		meta.create_gate("mul", |v_cells| {
+			let mul_s = v_cells.query_selector(mul_selector);
 			let x_limb_exps = x_limbs.map(|x| v_cells.query_advice(x, Rotation::cur()));
 			let y_limb_exps = y_limbs.map(|y| v_cells.query_advice(y, Rotation::cur()));
-			let mul_q_exp = quotient.map(|x| query_advice(x, Rotation::cur()));
+			let mul_q_exp = quotient.map(|x| v_cells.query_advice(x, Rotation::cur()));
 			let result_exps = x_limbs.map(|x| v_cells.query_advice(x, Rotation::next()));
 			let residues_exps: Vec<Expression<N>> =
-				residues.iter().map(|x| v_cells.query_advice(x, Rotation::cur())).collect();
+				residues.iter().map(|x| v_cells.query_advice(*x, Rotation::cur())).collect();
 
-			let mut t = [Expression::Constant(N::zero()); NUM_LIMBS];
+			let mut t = [(); NUM_LIMBS].map(|_| Expression::Constant(N::zero()));
 			for k in 0..NUM_LIMBS {
 				for i in 0..=k {
 					let j = k - i;
-					t[i + j] = t[i + j] + x_limbs[i] * y_limbs[j] + p_prime[i] * q[j];
+					t[i + j] = t[i + j].clone()
+						+ x_limb_exps[i].clone() * y_limb_exps[j].clone()
+						+ mul_q_exp[j].clone() * p_prime[i];
 				}
 			}
 
 			// REDUCTION CONSTRAINTS
-			P::constrain_binary_crt_exp(t, result_exps, residues_exps);
+			let mut constraints =
+				P::constrain_binary_crt_exp(t, result_exps.clone(), residues_exps);
+
+			let native_constraints = P::compose_exp(x_limb_exps) * P::compose_exp(y_limb_exps)
+				- P::compose_exp(mul_q_exp) * p_in_n
+				- P::compose_exp(result_exps);
+			constraints.push(native_constraints);
+
+			constraints.iter().map(|x| mul_s.clone() * x.clone()).collect::<Vec<Expression<N>>>()
 		});
 
 		IntegerConfig {
@@ -122,5 +118,66 @@ where
 			add_selector,
 			mul_selector,
 		}
+	}
+
+	/// Assign cells for add operation.
+	pub fn add(
+		&self, x_limbs: [AssignedCell<N, N>; NUM_LIMBS], y_limbs: [AssignedCell<N, N>; NUM_LIMBS],
+		rw: ReductionWitness<W, N, NUM_LIMBS, NUM_BITS, P>, config: IntegerConfig<NUM_LIMBS>,
+		mut layouter: impl Layouter<N>,
+	) -> Result<[AssignedCell<N, N>; NUM_LIMBS], Error> {
+		layouter.assign_region(
+			|| "add_operation",
+			|mut region: Region<'_, N>| {
+				config.add_selector.enable(&mut region, 0);
+
+				let mut assigned_x_limbs: [Option<AssignedCell<N, N>>; NUM_LIMBS] =
+					[None; NUM_LIMBS];
+				let mut assigned_y_limbs: [Option<AssignedCell<N, N>>; NUM_LIMBS] =
+					[None; NUM_LIMBS];
+				let mut assigned_intermediate: [Option<AssignedCell<N, N>>; NUM_LIMBS] =
+					[None; NUM_LIMBS];
+
+				for i in 0..NUM_LIMBS {
+					assigned_x_limbs = Some(x_limbs[i].copy_advice(
+						|| format!("x_limb_{}", i),
+						&mut region,
+						config.x_limbs[i],
+						0,
+					)?);
+
+					assigned_y_limbs = Some(y_limbs[i].copy_advice(
+						|| format!("y_limb_{}", i),
+						&mut region,
+						config.y_limbs[i],
+						0,
+					)?);
+
+					assigned_intermediate = Some(region.assign_advice(
+						|| format!("intermediate_{}", i),
+						&mut region,
+						config.intermediate[i],
+						0,
+						rw.
+					)?)
+				}
+
+				let mut assigned_residues = Vec::new();
+				for i in 0..residues.len() {
+					assigned_residues.push(residues[i].copy_advice(
+						|| format!("residues_{}", i),
+						&mut region,
+						config.residues[i],
+						0,
+					)?)
+				}
+
+				let assigned_x_limbs = assigned_x_limbs.map(|x| x.unwrap());
+				let assigned_y_limbs = assigned_y_limbs.map(|y| y.unwrap());
+				let assigned_quotient =
+					quotient.copy_advice(|| "quotient", &mut region, config.quotient[0], 0)?;
+				let assigned_intermediate = assigned_intermediate.map(|x| x.unwrap());
+			},
+		)
 	}
 }
--- circuit/src/wrong/rns.rs
+++ circuit/src/wrong/rns.rs
@@ -78,6 +78,8 @@ pub trait RnsParams<W: FieldExt, N: FieldExt, const NUM_LIMBS: usize, const NUM_
 	fn native_modulus() -> BigUint;
 	/// Returns Base (Wrong) Field modulus [`Fq`] from Bn256.
 	fn wrong_modulus() -> BigUint;
+	/// Returns wrong modulus in native modulus.
+	fn wrong_modulus_in_native_modulus() -> N;
 	/// Returns negative Base (Wrong) Field as decomposed.
 	fn negative_wrong_modulus_decomposed() -> [N; NUM_LIMBS];
 	/// Returns right shifters.
@@ -91,10 +93,17 @@ pub trait RnsParams<W: FieldExt, N: FieldExt, const NUM_LIMBS: usize, const NUM_
 	/// Returns `quotient` and `remainder` for the add operation.
 	fn construct_add_qr(a_bn: BigUint, b_bn: BigUint) -> (N, [N; NUM_LIMBS]);
 	/// Constraint for the binary part of `Chinese Remainder Theorem`.
-	fn constrain_binary_crt(t: [N; 4], result: [N; 4], residues: Vec<N>) -> bool;
+	fn constrain_binary_crt(t: [N; NUM_LIMBS], result: [N; NUM_LIMBS], residues: Vec<N>) -> bool;
+	/// Constraint for the binary part of `Chinese Remainder Theorem` using
+	/// Expressions.
 	fn constrain_binary_crt_exp(
-		t: [Expression<Fr>; 4], result: [Expression<Fr>; 4], residues: Vec<Expression<Fr>>,
-	) -> Vec<Expression<Fr>>;
+		t: [Expression<N>; NUM_LIMBS], result: [Expression<N>; NUM_LIMBS],
+		residues: Vec<Expression<N>>,
+	) -> Vec<Expression<N>>;
+	/// Composes integer limbs into single [`FieldExt`] value.
+	fn compose(input: [N; NUM_LIMBS]) -> N;
+	/// Composes integer limbs as Expressions into single Expression value.
+	fn compose_exp(input: [Expression<N>; NUM_LIMBS]) -> Expression<N>;
 }
 
 /// Returns modulus of the [`FieldExt`] as [`BigUint`].
@@ -129,7 +138,7 @@ pub fn decompose_big<F: FieldExt, const NUM_LIMBS: usize, const BIT_LEN: usize>(
 }
 
 /// Returns [`BigUint`] by composing `limbs`.
-pub fn compose<const NUM_LIMBS: usize, const NUM_BITS: usize>(
+pub fn compose_big<const NUM_LIMBS: usize, const NUM_BITS: usize>(
 	input: [BigUint; NUM_LIMBS],
 ) -> BigUint {
 	let mut res = BigUint::zero();
@@ -156,6 +165,10 @@ impl RnsParams<Fq, Fr, 4, 68> for Bn256_4_68 {
 			"21888242871839275222246405745257275088696311157297823662689037894645226208583",
 		)
 		.unwrap()
+	}
+
+	fn wrong_modulus_in_native_modulus() -> Fr {
+		Fr::from_u128(147946756881789318990833708069417712966)
 	}
 
 	fn negative_wrong_modulus_decomposed() -> [Fr; 4] {
@@ -245,17 +258,40 @@ impl RnsParams<Fq, Fr, 4, 68> for Bn256_4_68 {
 	fn constrain_binary_crt_exp(
 		t: [Expression<Fr>; 4], result: [Expression<Fr>; 4], residues: Vec<Expression<Fr>>,
 	) -> Vec<Expression<Fr>> {
-		let mut v = Expression::Constant(N::zero());
+		let lsh_one = Self::left_shifters()[1];
+		let lsh_two = Self::left_shifters()[2];
+
+		let mut v = Expression::Constant(Fr::zero());
 		let mut constraints = Vec::new();
-		for i in (0..NUM_LIMBS).step_by(2) {
-			let (t_lo, t_hi) = (t[i], t[i + 1]);
-			let (r_lo, r_hi) = (result[i], result[i + 1]);
+		for i in (0..4).step_by(2) {
+			let (t_lo, t_hi) = (t[i].clone(), t[i + 1].clone());
+			let (r_lo, r_hi) = (result[i].clone(), result[i + 1].clone());
 			let res =
-				t_lo + t_hi * lsh_one - r_lo - r_hi * lsh_one - residues_exps[i / 2] * lsh_two + v;
-			v = residues[i / 2];
+				t_lo + t_hi * lsh_one - r_lo - r_hi * lsh_one - residues[i / 2].clone() * lsh_two
+					+ v;
+			v = residues[i / 2].clone();
 			constraints.push(res);
 		}
 
 		constraints
+	}
+
+	/// Composes integer limbs into single [`FieldExt`] value.
+	fn compose(input: [Fr; 4]) -> Fr {
+		let left_shifters = Self::left_shifters();
+		let mut sum = Fr::zero();
+		for i in 0..4 {
+			sum += input[i] * left_shifters[i];
+		}
+		sum
+	}
+
+	fn compose_exp(input: [Expression<Fr>; 4]) -> Expression<Fr> {
+		let left_shifters = Self::left_shifters();
+		let mut sum = Expression::Constant(Fr::zero());
+		for i in 0..4 {
+			sum = sum + input[i].clone() * left_shifters[i];
+		}
+		sum
 	}
 }
