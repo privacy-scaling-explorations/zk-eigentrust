782f04b9e398196462d0e28c24ab3a1bf3ea4654
4rmanc <ozgurarmancyigit@hotmail.com>
2022-12-20 14:29:53 +0300

    new tree design

--- circuit/src/merkle_tree/native.rs
+++ circuit/src/merkle_tree/native.rs
@@ -2,120 +2,118 @@ use std::marker::PhantomData;
 
 use halo2wrong::halo2::arithmetic::FieldExt;
 use num_traits::pow;
+use std::collections::HashMap;
 
-use crate::{
-	params::RoundParams,
-	poseidon::native::{sponge::PoseidonSponge, Poseidon},
-};
+use crate::{params::RoundParams, poseidon::native::Poseidon};
 const WIDTH: usize = 5;
 
 #[derive(Clone, Debug)]
+/// MerkleTree structure
 struct MerkleTree<F: FieldExt, P>
 where
 	P: RoundParams<F, WIDTH>,
 {
+	/// PhantomData for the params
 	_params: PhantomData<P>,
-	root: Option<F>,
-	nodes: Vec<Option<F>>,
+	/// Variables with level and value to represent nodes
+	nodes: HashMap<usize, Vec<F>>,
+	/// Height of the tree
 	height: usize,
-	first_leaf_index: usize,
-}
-
-#[derive(Clone)]
-struct Path<F: FieldExt, P>
-where
-	P: RoundParams<F, WIDTH>,
-{
-	_params: PhantomData<P>,
-	value: F,
-	path_vec: Vec<F>,
 }
 
 impl<F: FieldExt, P> MerkleTree<F, P>
 where
 	P: RoundParams<F, WIDTH>,
 {
+	/// Create a new Merkle Tree with all nodes equal to the zero
 	fn new(height: usize) -> Self {
-		// An array starts from index 0, that is why we do -1 operation to find the
-		// first leaf index.
-		let first_leaf_index = pow(2, height) - 1;
-		// Calculating number of nodes is basically 2n - 1 (n is number of leaves)
-		let num_nodes = pow(2, height + 1) - 1;
-		let nodes = vec![None; num_nodes];
-		MerkleTree { _params: PhantomData, root: None, nodes, height, first_leaf_index }
+		// 0th level is the leaf level and the max level is the root level
+		let mut nodes = HashMap::new();
+		for i in 0..height + 1 {
+			let num_nodes = pow(2, height - i);
+			let nodes_vec = vec![F::zero(); num_nodes];
+			nodes.insert(i, nodes_vec);
+		}
+		MerkleTree { _params: PhantomData, nodes, height }
 	}
 
-	fn add_leaves(&mut self, values: Vec<F>) -> &Self {
+	/// Put values to the given level of the tree
+	fn put_values(&mut self, values: Vec<F>, level: usize) -> &Self {
 		for i in 0..values.len() {
-			self.nodes[self.first_leaf_index + i] = Some(values[i]);
+			self.nodes.get_mut(&level).unwrap()[i] = values[i];
 		}
 		self
 	}
 
+	/// Build tree's nodes and the root from given empty tree with only leaves
 	fn build_tree(&mut self) -> &Self {
-		if self.height > 0 {
-			let mut new_tree = MerkleTree::<F, P>::new(self.height - 1);
-			let first_leaf_index = (new_tree.first_leaf_index * 2) + 1;
+		for level in 0..self.height {
 			let mut hash = Vec::new();
-			for i in 0..first_leaf_index {
+			for i in 0..self.nodes[&level].len() {
 				if i % 2 == 0 {
 					let hasher: Poseidon<F, WIDTH, P> = Poseidon::new([
-						self.nodes[first_leaf_index + i].unwrap(),
-						self.nodes[first_leaf_index + i + 1].unwrap(),
+						self.nodes[&level][i],
+						self.nodes[&level][i + 1],
 						F::zero(),
 						F::zero(),
 						F::zero(),
 					]);
-					let hashes = hasher.permute();
-
-					hash.push(hashes[0] + hashes[1]);
+					hash.push(hasher.permute()[0]);
 				}
 			}
-			new_tree.add_leaves(hash.clone());
-			for i in new_tree.first_leaf_index..new_tree.nodes.len() {
-				self.nodes[i] = new_tree.nodes[i];
-			}
-			self.height = new_tree.height;
-			self.build_tree();
-			self.height += 1;
+			self.put_values(hash, level + 1);
 		}
-		self.root = self.nodes[0];
 		self
 	}
 
+	/// Find path for the given value to the root
 	fn find_path(&mut self, value: F) -> Path<F, P> {
-		let mut value_index = None;
-		for i in (self.first_leaf_index - 1)..self.nodes.len() {
-			if value == self.nodes[i].unwrap() {
-				value_index = Some(i);
+		let mut value_index = 0;
+		for i in 0..self.nodes[&0].len() {
+			if value == self.nodes[&0][i] {
+				value_index = i;
 				break;
 			}
 		}
-
 		let mut path_vec: Vec<F> = Vec::new();
-		let mut j = value_index.unwrap();
+		let mut j = value_index;
 		// Childs for a parent node is 2n and 2n - 1.
 		// Reverse is (n / 2) and (n / 2 - 1).
-		for i in 0..self.height {
-			if j % 2 == 0 {
-				path_vec.push(self.nodes[j - 1].unwrap());
-				path_vec.push(self.nodes[j].unwrap());
-				j = j / 2 - 1;
+
+		for level in 0..self.height {
+			if j % 2 == 1 {
+				path_vec.push(self.nodes[&level][j - 1]);
+				path_vec.push(self.nodes[&level][j]);
 			} else {
-				path_vec.push(self.nodes[j].unwrap());
-				path_vec.push(self.nodes[j + 1].unwrap());
-				j = j / 2;
+				path_vec.push(self.nodes[&level][j]);
+				path_vec.push(self.nodes[&level][j + 1]);
 			}
+			j = j / 2;
 		}
-		path_vec.push(self.root.unwrap());
+		path_vec.push(self.nodes[&self.height][0]);
 		Path { path_vec, value, _params: PhantomData }
 	}
+}
+
+#[derive(Clone)]
+/// Path structure
+struct Path<F: FieldExt, P>
+where
+	P: RoundParams<F, WIDTH>,
+{
+	/// PhantomData for the params
+	_params: PhantomData<P>,
+	/// Based on value for the path
+	value: F,
+	/// Vector that keeps the path
+	path_vec: Vec<F>,
 }
 
 impl<F: FieldExt, P> Path<F, P>
 where
 	P: RoundParams<F, WIDTH>,
 {
+	/// Sanity check for the path vector
 	fn verify(&self) -> bool {
 		let mut is_satisfied = true;
 		for i in 0..self.path_vec.len() - 2 {
@@ -127,8 +125,7 @@ where
 					F::zero(),
 					F::zero(),
 				]);
-				let hashes = hasher.permute();
-				is_satisfied = is_satisfied | self.path_vec.contains(&(hashes[0] + hashes[1]));
+				is_satisfied = is_satisfied | self.path_vec.contains(&(hasher.permute()[0]));
 			}
 		}
 		is_satisfied
@@ -145,22 +142,33 @@ mod test {
 
 	#[test]
 	fn should_build_tree_and_find_path() {
+		// Testing build_tree and find_path functions
 		let rng = &mut thread_rng();
-		let mut merkle = MerkleTree::<Fr, Params>::new(3);
+		let mut merkle = MerkleTree::<Fr, Params>::new(4);
 		let value = Fr::random(rng.clone());
-		merkle.add_leaves(vec![
-			Fr::random(rng.clone()),
-			Fr::random(rng.clone()),
-			Fr::random(rng.clone()),
-			Fr::random(rng.clone()),
-			Fr::random(rng.clone()),
-			value,
-			Fr::random(rng.clone()),
-			Fr::random(rng.clone()),
-		]);
+		// Test the tree with 9 values while it can take 16
+		merkle.put_values(
+			vec![
+				Fr::random(rng.clone()),
+				Fr::random(rng.clone()),
+				Fr::random(rng.clone()),
+				Fr::random(rng.clone()),
+				Fr::random(rng.clone()),
+				Fr::random(rng.clone()),
+				Fr::random(rng.clone()),
+				Fr::random(rng.clone()),
+				value,
+			],
+			0,
+		);
+
 		merkle.build_tree();
 		let path = merkle.find_path(value);
 		assert!(path.verify());
-		assert_eq!(path.path_vec[6], merkle.root.unwrap());
+		// Assert last element of the array and the root of the tree
+		assert_eq!(
+			path.path_vec[(2 * merkle.height)],
+			merkle.nodes[&merkle.height][0]
+		);
 	}
 }
