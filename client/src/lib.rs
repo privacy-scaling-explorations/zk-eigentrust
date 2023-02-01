80453826e5c1f5d21125c953b93cb35590373443
lazovicff <lazovicff@gmail.com>
2023-02-01 16:43:12 +0100

    Fixed attest test

--- client/src/lib.rs
+++ client/src/lib.rs
@@ -63,52 +63,51 @@ impl EigenTrustClient {
 			sk_vec.try_into().map_err(|_| ClientError::DecodeError)?;
 		let user_publics = user_secrets.map(|s| s.public());
 
-		// let sk0_bytes_vec = bs58::decode(&self.config.secret_key[0]).into_vec();
-		// let sk1_bytes_vec = bs58::decode(&self.config.secret_key[1]).into_vec();
+		let sk0_bytes_vec = bs58::decode(&self.config.secret_key[0]).into_vec();
+		let sk1_bytes_vec = bs58::decode(&self.config.secret_key[1]).into_vec();
 
-		// let sk0_bytes = sk0_bytes_vec.map_err(|_| ClientError::DecodeError)?;
-		// let sk1_bytes = sk1_bytes_vec.map_err(|_| ClientError::DecodeError)?;
+		let sk0_bytes = sk0_bytes_vec.map_err(|_| ClientError::DecodeError)?;
+		let sk1_bytes = sk1_bytes_vec.map_err(|_| ClientError::DecodeError)?;
 
-		// let mut sk0: [u8; 32] = [0; 32];
-		// sk0[..].copy_from_slice(&sk0_bytes);
+		let mut sk0: [u8; 32] = [0; 32];
+		sk0[..].copy_from_slice(&sk0_bytes);
 
-		// let mut sk1: [u8; 32] = [0; 32];
-		// sk1[..].copy_from_slice(&sk1_bytes);
+		let mut sk1: [u8; 32] = [0; 32];
+		sk1[..].copy_from_slice(&sk1_bytes);
 
-		// let sk = SecretKey::from_raw([sk0, sk1]);
-		// let pk = sk.public();
+		let sk = SecretKey::from_raw([sk0, sk1]);
+		let pk = sk.public();
 
-		// let ops = self.config.ops.map(|x| Scalar::from_u128(x));
+		let ops = self.config.ops.map(|x| Scalar::from_u128(x));
 
-		// let (pks_hash, message_hash) =
-		// 	calculate_message_hash::<NUM_NEIGHBOURS, 1>(user_publics.to_vec(),
-		// vec![ops.to_vec()]);
+		let (pks_hash, message_hash) =
+			calculate_message_hash::<NUM_NEIGHBOURS, 1>(user_publics.to_vec(), vec![ops.to_vec()]);
 
-		// let sig = sign(&sk, &pk, message_hash[0]);
+		let sig = sign(&sk, &pk, message_hash[0]);
 
-		// let att = Attestation::new(sig, pk, user_publics.to_vec(), ops.to_vec());
-		// let att_data = AttestationData::from(att);
-		// let bytes = att_data.to_bytes();
+		let att = Attestation::new(sig, pk, user_publics.to_vec(), ops.to_vec());
+		let att_data = AttestationData::from(att);
+		let bytes = att_data.to_bytes();
 
-		// let as_address_res = self.config.as_address.parse::<Address>();
-		// let as_address = as_address_res.map_err(|_| ClientError::ParseError)?;
-		// let as_contract = AttStation::new(as_address, self.client.clone());
+		let as_address_res = self.config.as_address.parse::<Address>();
+		let as_address = as_address_res.map_err(|_| ClientError::ParseError)?;
+		let as_contract = AttStation::new(as_address, self.client.clone());
 
-		// let as_data = AttData(
-		// 	Address::zero(),
-		// 	pks_hash.to_bytes(),
-		// 	Bytes::from(bytes.clone()),
-		// );
-		// let as_data_vec = vec![as_data];
+		let as_data = AttData(
+			Address::zero(),
+			pks_hash.to_bytes(),
+			Bytes::from(bytes.clone()),
+		);
+		let as_data_vec = vec![as_data];
 
-		// let tx_call = as_contract.attest(as_data_vec);
-		// let tx_res = tx_call.send();
-		// let tx = tx_res.await.map_err(|_| ClientError::TxError)?;
-		// let res = tx.await.map_err(|_| ClientError::TxError)?;
+		let tx_call = as_contract.attest(as_data_vec);
+		let tx_res = tx_call.send();
+		let tx = tx_res.await.map_err(|_| ClientError::TxError)?;
+		let res = tx.await.map_err(|_| ClientError::TxError)?;
 
-		// if let Some(receipt) = res {
-		// 	println!("Transaction status: {:?}", receipt.status);
-		// }
+		if let Some(receipt) = res {
+			println!("Transaction status: {:?}", receipt.status);
+		}
 
 		Ok(())
 	}
@@ -116,13 +115,17 @@ impl EigenTrustClient {
 
 #[cfg(test)]
 mod test {
-	use crate::{ClientConfig, EigenTrustClient};
+	use crate::{utils::deploy, ClientConfig, EigenTrustClient};
 	use eigen_trust_protocol::manager::NUM_NEIGHBOURS;
 	use ethers::utils::Anvil;
 
 	#[tokio::test]
 	async fn should_add_attestation() {
 		let anvil = Anvil::new().spawn();
+		let mnemonic = "test test test test test test test test test test test junk".to_string();
+		let node_url = anvil.endpoint();
+		let address = deploy(&mnemonic, &node_url).await.unwrap();
+		let address_string = format!("{:?}", address);
 
 		let dummy_user = [
 			"Alice".to_string(),
@@ -137,9 +140,9 @@ mod test {
 				"2L9bbXNEayuRMMbrWFynPtgkrXH1iBdfryRH9Soa8M67".to_string(),
 				"9rBeBVtbN2MkHDTpeAouqkMWNFJC6Bxb6bXH9jUueWaF".to_string(),
 			],
-			as_address: String::new(),
-			mnemonic: "test test test test test test test test test test test junk".to_string(),
-			ethereum_node_url: anvil.endpoint(),
+			as_address: address_string,
+			mnemonic,
+			ethereum_node_url: node_url,
 		};
 
 		let et_client = EigenTrustClient::new(config, user_secrets_raw);
