a8a612f78b46e2ef92e5bdbf8fd3a4081ebecddf
lazovicff <lazovicff@gmail.com>
2023-02-23 15:33:37 +0100

    More name updates

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,2 +1,2 @@
-cargo test -p eigen-trust-protocol --release -- --test-threads=1
+cargo test -p eigen-trust-server --release -- --test-threads=1
 cargo test -p eigen-trust-circuit --release -- --test-threads=1
