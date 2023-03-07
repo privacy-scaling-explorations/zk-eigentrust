b8dc89e7c490903c452416d1f47997823f66478d
lazovicff <lazovicff@gmail.com>
2023-03-07 11:15:06 +0100

    Running tests for client inside CI

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,2 +1,3 @@
 cargo test -p eigen-trust-server --release -- --test-threads=1
+cargo test -p eigen-trust-client --release -- --test-threads=1
 cargo test -p eigen-trust-circuit --release -- --test-threads=1
