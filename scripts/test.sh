c89b418c289aa2c395e278ece2482c608d125dc1
lazovicff <lazovicff@gmail.com>
2023-03-09 12:00:24 +0100

    checkout to use the repo

--- scripts/test.sh
+++
@@ -1,3 +0,0 @@
-cargo test -p eigen-trust-server --release -- --test-threads=1
-cargo test -p eigen-trust-client --release -- --test-threads=1
-cargo test -p eigen-trust-circuit --release -- --test-threads=1
