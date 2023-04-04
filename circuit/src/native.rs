1c6a577903381a86b943950a523db2bc222abb6c
duguorong <dudurong009@gmail.com>
2023-04-04 22:56:10 +0800

    Update the "filter_peers" in "EigenTrustSet" (circuit/src/native.rs)
    Implement the logic of distributing scores to other peers

--- circuit/src/native.rs
+++ circuit/src/native.rs
@@ -1,4 +1,4 @@
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
 
 use crate::eddsa::native::{PublicKey, Signature};
 use halo2::{
@@ -85,48 +85,7 @@ impl EigenTrustSet {
 	}
 
 	pub fn converge(&mut self) -> [Fr; NUM_NEIGHBOURS] {
-		let mut filtered_set = self.set.clone();
-		let mut filtered_ops: HashMap<PublicKey, Opinion> = HashMap::new();
-
-		// Validity checks
-
-		// Nullify opinion scores that are given to wrong member at specific index
-		for i in 0..NUM_NEIGHBOURS {
-			let (pk, credits) = filtered_set[i];
-
-			if pk == PublicKey::default() || credits == Fr::zero() {
-				assert!(self.ops.get(&pk).is_none());
-			} else {
-				let mut ops_i = self.ops.get(&pk).unwrap().clone();
-
-				for j in 0..NUM_NEIGHBOURS {
-					let (pk_j, score) = ops_i.scores[j].clone();
-					let true_score =
-						if j != i && self.set[j].0 == pk_j { score } else { Fr::zero() };
-					ops_i.scores[j].1 = true_score;
-				}
-
-				filtered_ops.insert(pk, ops_i);
-			}
-		}
-
-		//  Filter the set -- set after we invalidated invalid peers:
-		// 		- Peers that dont have at least one valid score
-		for i in 0..NUM_NEIGHBOURS {
-			let (pk, credits) = filtered_set[i];
-
-			if !(pk == PublicKey::default() || credits == Fr::zero()) {
-				let ops_i = filtered_ops.get(&pk).unwrap();
-
-				let op_score_sum =
-					ops_i.scores.iter().fold(Fr::zero(), |acc, &(_, score)| acc + score);
-
-				if op_score_sum == Fr::zero() {
-					filtered_set[i] = (PublicKey::default(), Fr::zero());
-					filtered_ops.remove(&pk);
-				}
-			}
-		}
+		let (filtered_set, mut filtered_ops) = self.filter_peers();
 
 		// Normalize the opinion scores
 		// Distribute the credits(INITIAL_SCORE) to the valid opinion values
@@ -188,116 +147,126 @@ impl EigenTrustSet {
 		s
 	}
 
-	/// Filter out the lists of invalid peers from `self.set`.
-	///
-	/// Ouput: 2 vectors of indices of invalid peer public keys
-	///
-	/// Here, we use kinda recursive approach to filter out the direct-invalid
-	/// and indirect-invalid peers.
-	fn filter_invalid_peers(&mut self) -> (Vec<usize>, Vec<usize>) {
-		let mut direct_invalid_peer_ids: Vec<usize> = vec![];
-		let mut indirect_invalid_peer_ids: Vec<usize> = vec![];
+	fn filter_peers(
+		&self,
+	) -> (
+		[(PublicKey, Fr); NUM_NEIGHBOURS],
+		HashMap<PublicKey, Opinion>,
+	) {
+		let mut filtered_set: [(PublicKey, Fr); NUM_NEIGHBOURS] = self.set.clone();
+		let mut filtered_ops: HashMap<PublicKey, Opinion> = HashMap::new();
 
-		let mut invalid_peers_cnt = 0;
+		let mut valid_peers: Vec<PublicKey> = vec![];
 
-		// Direct Invalid - all opinions are given to invalid peers
+		// Convert the peer who didn't sign his opinion, to default peer
+		//
+		// Example:
+		//  In this set, peer3 didn't sign the opinion.
+		//		[(p1, 10), (p2, 10), (p3, 0)]
+		//   => [(p1, 10), (p2, 10), (null, 0)]
+		// Here, "null" means default peer
 		for i in 0..NUM_NEIGHBOURS {
-			let (pk, credits) = self.set[i].clone();
-
-			// No credits(initial score) means that the peer did not update his opinion.
-			// In other words, the peer did not sign his opinion.
-			if pk != PublicKey::default() && credits == Fr::zero() {
-				direct_invalid_peer_ids.push(i);
+			let (pk, credits) = filtered_set[i].clone();
+			// If no credits, the peer did not sign his opinion
+			if credits == Fr::zero() {
+				filtered_set[i] = (PublicKey::default(), Fr::zero());
 			} else {
-				if pk == PublicKey::default() {
-					continue;
-				} else {
-					let mut ops_i = self.ops.get(&pk).unwrap().clone();
+				valid_peers.push(pk);
+			}
+		}
 
-					for j in 0..NUM_NEIGHBOURS {
-						let (pk_j, score) = ops_i.scores[j].clone();
+		// Distribute the scores to valid peers
+		for i in 0..NUM_NEIGHBOURS {
+			let (pk_i, _) = filtered_set[i].clone();
+			if pk_i != PublicKey::default() {
+				let mut ops_i = self.ops.get(&pk_i).unwrap().clone();
 
-						if score == Fr::zero() {
-							continue;
-						} else {
-							if j != i && self.set[j].0 == pk_j && pk_j != PublicKey::default() {
-								ops_i.scores[j].1 = score;
-							} else {
-								ops_i.scores[j].1 = Fr::zero();
-							};
+				// Give zero score for peers
+				//   - giving scores to itself
+				// 	 - giving scores to default peer
+				//
+				// Example:
+				//   Peer1 opinion
+				//		[(p1, 10), (p2, 10), (null, 10)]
+				// 	 => [(p1,  0), (p2, 10), (null,  0)]
+				//
+				for j in 0..NUM_NEIGHBOURS {
+					let (pk_j, score) = ops_i.scores[j].clone();
+					let true_pk = if i == j {
+						pk_i
+					} else if pk_j == pk_i {
+						PublicKey::default()
+					} else {
+						pk_j
+					};
+					let true_score = if true_pk == pk_i || true_pk == PublicKey::default() {
+						Fr::zero()
+					} else {
+						score
+					};
+					ops_i.scores[j] = (true_pk, true_score);
+				}
+
+				// Update the opinion array - pairs of (key, score)
+				//
+				// Example 1:
+				// 	filtered_set => [p1, null, p3]
+				//	Peer1 opinion
+				// 		[(p1, 0), (p6, 10),  (p3, 10)]
+				//   => [(p1, 0), (null, 0), (p3, 10)]
+				//
+				// Example 2:
+				// 	filtered_set => [p1, p2, p3]
+				//	Peer1 opinion
+				// 		[(p1, 0), (p3, 10), (null, 0)]
+				//   => [(p1, 0), (p2, 0),  (p3, 0)]
+				let op_score_sum =
+					ops_i.scores.iter().fold(Fr::zero(), |acc, &(_, score)| acc + score);
+				if op_score_sum == Fr::zero() {
+					panic!("The peer {i} didn't give any score to other peers!");
+				}
+				for j in 0..NUM_NEIGHBOURS {
+					let (set_pk_j, _) = filtered_set[j];
+					let (op_pk_j, _) = ops_i.scores[j].clone();
+
+					if set_pk_j == PublicKey::default() {
+						ops_i.scores[j] = (PublicKey::default(), Fr::zero());
+					} else {
+						if op_pk_j != set_pk_j {
+							ops_i.scores[j] = (set_pk_j, Fr::zero());
 						}
 					}
-
-					if ops_i.scores.iter().all(|(_, score)| *score == Fr::zero()) {
-						direct_invalid_peer_ids.push(i);
-					}
 				}
-			}
-		}
-		invalid_peers_cnt += direct_invalid_peer_ids.len();
 
-		// Indirect Invalid - Opinions that became invalid
-		// 					after we marked Direct-Invalid opinions
-		loop {
-			for i in 0..NUM_NEIGHBOURS {
-				let (pk, _) = self.set[i].clone();
-
-				if pk == PublicKey::default()
-					|| direct_invalid_peer_ids.contains(&i)
-					|| indirect_invalid_peer_ids.contains(&i)
-				{
-					continue;
-				} else {
-					let mut ops_i = self.ops.get(&pk).unwrap().clone();
-
+				// Distribute the scores
+				//
+				// Example 1:
+				// 	filtered_set => [p1, p2, p3]
+				//	Peer1 opinion
+				// 		[(p1, 0), (p2, 0), (p3, 10)]
+				//   => [(p1, 0), (p2, 0), (p3, 10)]
+				//
+				// Example 2: (Cont.d from above example 2)
+				// 	filtered_set => [p1, p2, p3]
+				//	Peer1 opinion
+				//      [(p1, 0), (p2, 0), (p3, 0)]
+				//   => [(p1, 0), (p2, 1), (p3, 1)]
+				let new_op_score_sum =
+					ops_i.scores.iter().fold(Fr::zero(), |acc, &(_, score)| acc + score);
+				if new_op_score_sum == Fr::zero() {
 					for j in 0..NUM_NEIGHBOURS {
-						let (_, score) = ops_i.scores[j].clone();
-
-						if score == Fr::zero() {
-							continue;
-						} else {
-							if direct_invalid_peer_ids.contains(&j)
-								|| indirect_invalid_peer_ids.contains(&j)
-							{
-								ops_i.scores[j].1 = Fr::zero();
-							}
+						let (pk_j, _) = ops_i.scores[j].clone();
+						if i != j && valid_peers.contains(&pk_j) {
+							ops_i.scores[j] = (pk_j, Fr::from(1));
 						}
 					}
-
-					if ops_i.scores.iter().all(|(_, score)| *score == Fr::zero()) {
-						indirect_invalid_peer_ids.push(i);
-					}
 				}
-			}
-			println!(
-				"Invalid peers: {}",
-				direct_invalid_peer_ids.len() + indirect_invalid_peer_ids.len()
-			);
 
-			// If no more invalid peers added, quits the loop
-			let new_invalid_peers_cnt =
-				direct_invalid_peer_ids.len() + indirect_invalid_peer_ids.len();
-			if new_invalid_peers_cnt == invalid_peers_cnt {
-				break;
-			} else {
-				invalid_peers_cnt = direct_invalid_peer_ids.len() + indirect_invalid_peer_ids.len()
+				filtered_ops.insert(pk_i, ops_i);
 			}
 		}
 
-		// Update the `ops_valid` map
-		for i in 0..NUM_NEIGHBOURS {
-			let (pk, _) = self.set[i].clone();
-			if pk == PublicKey::default() {
-				self.ops_validity.insert(pk, None);
-			} else if direct_invalid_peer_ids.contains(&i) || indirect_invalid_peer_ids.contains(&i)
-			{
-				self.ops_validity.insert(pk, Some(false));
-			} else {
-				self.ops_validity.insert(pk, Some(true));
-			}
-		}
-
-		(direct_invalid_peer_ids, indirect_invalid_peer_ids)
+		(filtered_set, filtered_ops)
 	}
 }
 
@@ -306,10 +275,23 @@ mod test {
 	use super::{EigenTrustSet, Opinion, INITIAL_SCORE, NUM_NEIGHBOURS};
 	use crate::{
 		calculate_message_hash,
-		eddsa::native::{sign, PublicKey, SecretKey},
+		eddsa::native::{sign, PublicKey, SecretKey, Signature},
 	};
 	use halo2::halo2curves::{bn256::Fr, FieldExt};
 	use rand::thread_rng;
+
+	fn sign_opinion(
+		sk: &SecretKey, pk: &PublicKey, pks: &[PublicKey; NUM_NEIGHBOURS],
+		scores: &[Fr; NUM_NEIGHBOURS],
+	) -> Opinion {
+		let (_, message_hashes) =
+			calculate_message_hash::<NUM_NEIGHBOURS, 1>(pks.to_vec(), vec![scores.to_vec()]);
+		let sig = sign(sk, pk, message_hashes[0]);
+
+		let scores = pks.zip(*scores);
+		let op = Opinion::new(sig, message_hashes[0], scores);
+		op
+	}
 
 	#[test]
 	#[should_panic]
@@ -379,14 +361,9 @@ mod test {
 			Fr::zero(),
 			Fr::zero(),
 		];
-		let (_, message_hashes) =
-			calculate_message_hash::<NUM_NEIGHBOURS, 1>(pks.to_vec(), vec![scores.to_vec()]);
-		let sig = sign(&sk1, &pk1, message_hashes[0]);
+		let op1 = sign_opinion(&sk1, &pk1, &pks, &scores);
 
-		let scores = pks.zip(scores);
-		let op = Opinion::new(sig, message_hashes[0], scores);
-
-		set.update_op(pk1, op);
+		set.update_op(pk1, op1);
 
 		set.converge();
 	}
@@ -423,14 +400,9 @@ mod test {
 			Fr::zero(),
 			Fr::zero(),
 		];
-		let (_, message_hashes) =
-			calculate_message_hash::<NUM_NEIGHBOURS, 1>(pks.to_vec(), vec![scores.to_vec()]);
-		let sig = sign(&sk1, &pk1, message_hashes[0]);
+		let op1 = sign_opinion(&sk1, &pk1, &pks, &scores);
 
-		let scores = pks.zip(scores);
-		let op = Opinion::new(sig, message_hashes[0], scores);
-
-		set.update_op(pk1, op);
+		set.update_op(pk1, op1);
 
 		// Peer2(pk2) signs the opinion
 		let pks = [
@@ -449,14 +421,9 @@ mod test {
 			Fr::zero(),
 			Fr::zero(),
 		];
-		let (_, message_hashes) =
-			calculate_message_hash::<NUM_NEIGHBOURS, 1>(pks.to_vec(), vec![scores.to_vec()]);
-		let sig = sign(&sk2, &pk2, message_hashes[0]);
+		let op2 = sign_opinion(&sk2, &pk2, &pks, &scores);
 
-		let scores = pks.zip(scores);
-		let op = Opinion::new(sig, message_hashes[0], scores);
-
-		set.update_op(pk2, op);
+		set.update_op(pk2, op2);
 
 		set.converge();
 	}
@@ -482,38 +449,23 @@ mod test {
 		// Peer1(pk1) signs the opinion
 		let pks = [pk1, pk2, pk3, PublicKey::default(), PublicKey::default(), PublicKey::default()];
 		let scores = [Fr::zero(), Fr::from(300), Fr::from(700), Fr::zero(), Fr::zero(), Fr::zero()];
-		let (_, message_hashes) =
-			calculate_message_hash::<NUM_NEIGHBOURS, 1>(pks.to_vec(), vec![scores.to_vec()]);
-		let sig = sign(&sk1, &pk1, message_hashes[0]);
+		let op1 = sign_opinion(&sk1, &pk1, &pks, &scores);
 
-		let scores = pks.zip(scores);
-		let op = Opinion::new(sig, message_hashes[0], scores);
-
-		set.update_op(pk1, op);
+		set.update_op(pk1, op1);
 
 		// Peer2(pk2) signs the opinion
 		let pks = [pk1, pk2, pk3, PublicKey::default(), PublicKey::default(), PublicKey::default()];
 		let scores = [Fr::from(600), Fr::zero(), Fr::from(400), Fr::zero(), Fr::zero(), Fr::zero()];
-		let (_, message_hashes) =
-			calculate_message_hash::<NUM_NEIGHBOURS, 1>(pks.to_vec(), vec![scores.to_vec()]);
-		let sig = sign(&sk2, &pk2, message_hashes[0]);
+		let op2 = sign_opinion(&sk2, &pk2, &pks, &scores);
 
-		let scores = pks.zip(scores);
-		let op = Opinion::new(sig, message_hashes[0], scores);
-
-		set.update_op(pk2, op);
+		set.update_op(pk2, op2);
 
 		// Peer3(pk3) signs the opinion
 		let pks = [pk1, pk2, pk3, PublicKey::default(), PublicKey::default(), PublicKey::default()];
 		let scores = [Fr::from(600), Fr::from(400), Fr::zero(), Fr::zero(), Fr::zero(), Fr::zero()];
-		let (_, message_hashes) =
-			calculate_message_hash::<NUM_NEIGHBOURS, 1>(pks.to_vec(), vec![scores.to_vec()]);
-		let sig = sign(&sk3, &pk3, message_hashes[0]);
+		let op3 = sign_opinion(&sk3, &pk3, &pks, &scores);
 
-		let scores = pks.zip(scores);
-		let op = Opinion::new(sig, message_hashes[0], scores);
-
-		set.update_op(pk3, op);
+		set.update_op(pk3, op3);
 
 		set.converge();
 	}
@@ -539,26 +491,16 @@ mod test {
 		// Peer1(pk1) signs the opinion
 		let pks = [pk1, pk2, pk3, PublicKey::default(), PublicKey::default(), PublicKey::default()];
 		let scores = [Fr::zero(), Fr::from(300), Fr::from(700), Fr::zero(), Fr::zero(), Fr::zero()];
-		let (_, message_hashes) =
-			calculate_message_hash::<NUM_NEIGHBOURS, 1>(pks.to_vec(), vec![scores.to_vec()]);
-		let sig = sign(&sk1, &pk1, message_hashes[0]);
+		let op1 = sign_opinion(&sk1, &pk1, &pks, &scores);
 
-		let scores = pks.zip(scores);
-		let op = Opinion::new(sig, message_hashes[0], scores);
-
-		set.update_op(pk1, op);
+		set.update_op(pk1, op1);
 
 		// Peer2(pk2) signs the opinion
 		let pks = [pk1, pk2, pk3, PublicKey::default(), PublicKey::default(), PublicKey::default()];
 		let scores = [Fr::from(600), Fr::zero(), Fr::from(400), Fr::zero(), Fr::zero(), Fr::zero()];
-		let (_, message_hashes) =
-			calculate_message_hash::<NUM_NEIGHBOURS, 1>(pks.to_vec(), vec![scores.to_vec()]);
-		let sig = sign(&sk2, &pk2, message_hashes[0]);
+		let op2 = sign_opinion(&sk2, &pk2, &pks, &scores);
 
-		let scores = pks.zip(scores);
-		let op = Opinion::new(sig, message_hashes[0], scores);
-
-		set.update_op(pk2, op);
+		set.update_op(pk2, op2);
 
 		set.converge();
 	}
@@ -584,38 +526,23 @@ mod test {
 		// Peer1(pk1) signs the opinion
 		let pks = [pk1, pk2, pk3, PublicKey::default(), PublicKey::default(), PublicKey::default()];
 		let scores = [Fr::zero(), Fr::from(300), Fr::from(700), Fr::zero(), Fr::zero(), Fr::zero()];
-		let (_, message_hashes) =
-			calculate_message_hash::<NUM_NEIGHBOURS, 1>(pks.to_vec(), vec![scores.to_vec()]);
-		let sig = sign(&sk1, &pk1, message_hashes[0]);
+		let op1 = sign_opinion(&sk1, &pk1, &pks, &scores);
 
-		let scores = pks.zip(scores);
-		let op = Opinion::new(sig, message_hashes[0], scores);
-
-		set.update_op(pk1, op);
+		set.update_op(pk1, op1);
 
 		// Peer2(pk2) signs the opinion
 		let pks = [pk1, pk2, pk3, PublicKey::default(), PublicKey::default(), PublicKey::default()];
 		let scores = [Fr::from(600), Fr::zero(), Fr::from(400), Fr::zero(), Fr::zero(), Fr::zero()];
-		let (_, message_hashes) =
-			calculate_message_hash::<NUM_NEIGHBOURS, 1>(pks.to_vec(), vec![scores.to_vec()]);
-		let sig = sign(&sk2, &pk2, message_hashes[0]);
+		let op2 = sign_opinion(&sk2, &pk2, &pks, &scores);
 
-		let scores = pks.zip(scores);
-		let op = Opinion::new(sig, message_hashes[0], scores);
-
-		set.update_op(pk2, op);
+		set.update_op(pk2, op2);
 
 		// Peer3(pk3) signs the opinion
 		let pks = [pk1, pk2, pk3, PublicKey::default(), PublicKey::default(), PublicKey::default()];
 		let scores = [Fr::from(600), Fr::from(400), Fr::zero(), Fr::zero(), Fr::zero(), Fr::zero()];
-		let (_, message_hashes) =
-			calculate_message_hash::<NUM_NEIGHBOURS, 1>(pks.to_vec(), vec![scores.to_vec()]);
-		let sig = sign(&sk3, &pk3, message_hashes[0]);
+		let op3 = sign_opinion(&sk3, &pk3, &pks, &scores);
 
-		let scores = pks.zip(scores);
-		let op = Opinion::new(sig, message_hashes[0], scores);
-
-		set.update_op(pk3, op);
+		set.update_op(pk3, op3);
 
 		set.converge();
 
@@ -647,26 +574,16 @@ mod test {
 		// Peer1(pk1) signs the opinion
 		let pks = [pk1, pk2, pk3, PublicKey::default(), PublicKey::default(), PublicKey::default()];
 		let scores = [Fr::zero(), Fr::from(300), Fr::from(700), Fr::zero(), Fr::zero(), Fr::zero()];
-		let (_, message_hashes) =
-			calculate_message_hash::<NUM_NEIGHBOURS, 1>(pks.to_vec(), vec![scores.to_vec()]);
-		let sig = sign(&sk1, &pk1, message_hashes[0]);
+		let op1 = sign_opinion(&sk1, &pk1, &pks, &scores);
 
-		let scores = pks.zip(scores);
-		let op = Opinion::new(sig, message_hashes[0], scores);
-
-		set.update_op(pk1, op);
+		set.update_op(pk1, op1);
 
 		// Peer2(pk2) signs the opinion
 		let pks = [pk1, pk2, pk3, PublicKey::default(), PublicKey::default(), PublicKey::default()];
 		let scores = [Fr::from(600), Fr::zero(), Fr::from(400), Fr::zero(), Fr::zero(), Fr::zero()];
-		let (_, message_hashes) =
-			calculate_message_hash::<NUM_NEIGHBOURS, 1>(pks.to_vec(), vec![scores.to_vec()]);
-		let sig = sign(&sk2, &pk2, message_hashes[0]);
+		let op2 = sign_opinion(&sk2, &pk2, &pks, &scores);
 
-		let scores = pks.zip(scores);
-		let op = Opinion::new(sig, message_hashes[0], scores);
-
-		set.update_op(pk2, op);
+		set.update_op(pk2, op2);
 
 		set.converge();
 
@@ -677,11 +594,11 @@ mod test {
 	}
 
 	#[test]
-	fn test_filter_invalid_peers() {
-		//	Should filter out all the peers with following opinions.
+	fn test_filter_peers() {
+		//	Filter the peers with following opinions:
 		//			1	2	3	4	5	6
 		//		--------------------------
-		//		1	10	.	.	.	.	.
+		//		1	10	10	.	.	10	.
 		//		2	.	.	30	.	.	.
 		//		3	10	.	.	.	.	.
 		//		4	.	.	.	.	.	.
@@ -701,33 +618,18 @@ mod test {
 
 		// Peer1(pk1) signs the opinion
 		let pks = [pk1, pk2, pk3, PublicKey::default(), PublicKey::default(), pk8];
-		let scores = [Fr::from(10), Fr::zero(), Fr::zero(), Fr::zero(), Fr::zero(), Fr::zero()];
-		let (_, message_hashes) =
-			calculate_message_hash::<NUM_NEIGHBOURS, 1>(pks.to_vec(), vec![scores.to_vec()]);
-		let sig = sign(&sk1, &pk1, message_hashes[0]);
-
-		let scores = pks.zip(scores);
-		let op1 = Opinion::new(sig, message_hashes[0], scores);
+		let scores = [Fr::from(10), Fr::from(10), Fr::zero(), Fr::zero(), Fr::from(10), Fr::zero()];
+		let op1 = sign_opinion(&sk1, &pk1, &pks, &scores);
 
 		// Peer2(pk2) signs the opinion
 		let pks = [pk1, pk2, pk3, PublicKey::default(), PublicKey::default(), PublicKey::default()];
 		let scores = [Fr::zero(), Fr::zero(), Fr::from(30), Fr::zero(), Fr::zero(), Fr::zero()];
-		let (_, message_hashes) =
-			calculate_message_hash::<NUM_NEIGHBOURS, 1>(pks.to_vec(), vec![scores.to_vec()]);
-		let sig = sign(&sk2, &pk2, message_hashes[0]);
-
-		let scores = pks.zip(scores);
-		let op2 = Opinion::new(sig, message_hashes[0], scores);
+		let op2 = sign_opinion(&sk2, &pk2, &pks, &scores);
 
 		// Peer3(pk3) signs the opinion
 		let pks = [pk1, pk2, pk3, PublicKey::default(), PublicKey::default(), PublicKey::default()];
 		let scores = [Fr::from(10), Fr::zero(), Fr::zero(), Fr::zero(), Fr::zero(), Fr::zero()];
-		let (_, message_hashes) =
-			calculate_message_hash::<NUM_NEIGHBOURS, 1>(pks.to_vec(), vec![scores.to_vec()]);
-		let sig = sign(&sk3, &pk3, message_hashes[0]);
-
-		let scores = pks.zip(scores);
-		let op3 = Opinion::new(sig, message_hashes[0], scores);
+		let op3 = sign_opinion(&sk3, &pk3, &pks, &scores);
 
 		// Setup EigenTrustSet
 		let mut set = EigenTrustSet::new();
@@ -740,11 +642,11 @@ mod test {
 		set.update_op(pk2, op2);
 		set.update_op(pk3, op3);
 
-		let (direct_invalid_peer_indices, indirect_invalid_peer_indices) =
-			set.filter_invalid_peers();
-		assert!(
-			direct_invalid_peer_indices.len() + indirect_invalid_peer_indices.len() == 3,
-			"Should filter out all peers as invalid"
-		);
+		let (filtered_set, filtered_ops) = set.filter_peers();
+
+		let final_peers_cnt =
+			filtered_set.iter().filter(|&&(pk, _)| pk != PublicKey::default()).count();
+		let final_ops_cnt = filtered_ops.keys().count();
+		assert!(final_peers_cnt == final_ops_cnt);
 	}
 }
