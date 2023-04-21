d8c0ef14456a1c4e99cfa98c442407a78f91d49c
duguorong <dudurong009@gmail.com>
2023-04-21 17:16:16 +0800

    fix: enable the reputation sum check in native

--- circuit/src/dynamic_sets/native.rs
+++ circuit/src/dynamic_sets/native.rs
@@ -145,9 +145,7 @@ impl EigenTrustSet {
 
 		let sum_initial = filtered_set.iter().fold(Fr::zero(), |acc, &(_, score)| acc + score);
 		let sum_final = s.iter().fold(Fr::zero(), |acc, &(_, score)| acc + score);
-		// TODO: Make sure that the total amount of reputation stays the same after
-		// convergence
-		// assert!(sum_initial == sum_final);
+		assert!(sum_initial == sum_final);
 		println!("sum before: {:?}, sum after: {:?}", sum_initial, sum_final);
 
 		s
