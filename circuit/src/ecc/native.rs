d7905c160facb000975b88c28c48f82c1effdc0a
Akilesh Tangella <akileshtangella@Akileshs-MacBook-Pro.local>
2023-06-01 01:58:51 -0400

    fix fmt

--- circuit/src/ecc/native.rs
+++ circuit/src/ecc/native.rs
@@ -300,8 +300,7 @@ where
 
 		let mut aux_fins: Vec<EcPoint<W, N, NUM_LIMBS, NUM_BITS, P>> = vec![];
 		let aux_init = Self::to_sub();
-		let mut aux_fin =
-			Self::make_mul_aux_sliding_window::<S>(aux_init, sliding_window_usize);
+		let mut aux_fin = Self::make_mul_aux_sliding_window::<S>(aux_init, sliding_window_usize);
 		for i in 0..points.len() {
 			aux_fins.push(aux_fin.clone());
 			aux_fin = aux_fin.double();
