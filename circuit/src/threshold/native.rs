0f2fe060b0a88b5417589b8d9d60578ffaec6240
duguorong <dgr93930@gmail.com>
2023-06-06 15:43:03 +0800

    wip: fix the bound error

--- circuit/src/threshold/native.rs
+++ circuit/src/threshold/native.rs
@@ -5,15 +5,16 @@ use crate::{
 use num_rational::BigRational;
 
 /// Structure for threshold checks
-pub struct Threshold<F: FieldExt, const NUM_LIMBS: usize, const POWER_OF_TEN: usize> {
+pub struct Threshold<F: FieldExt + PartialOrd, const NUM_LIMBS: usize, const POWER_OF_TEN: usize> {
 	score: F,
 	ratio: BigRational,
 	threshold: F,
 }
 
-impl<F: FieldExt, const NUM_LIMBS: usize, const POWER_OF_TEN: usize>
+impl<F: FieldExt + PartialOrd, const NUM_LIMBS: usize, const POWER_OF_TEN: usize>
 	Threshold<F, NUM_LIMBS, POWER_OF_TEN>
 {
+	/// Create new instance
 	pub fn new(score: F, ratio: BigRational, threshold: F) -> Self {
 		Self { score, ratio, threshold }
 	}
@@ -22,9 +23,9 @@ impl<F: FieldExt, const NUM_LIMBS: usize, const POWER_OF_TEN: usize>
 	// TODO: Find `NUM_LIMBS` and `POWER_OF_TEN` for standardised decimal position
 	/// Method for checking the threshold for a given score
 	pub fn check_threshold(&self) -> ThresholdWitness<F, NUM_LIMBS> {
-		let &Threshold { score, ratio, threshold } = self.clone();
+		let Threshold { score, ratio, threshold } = self.clone();
 
-		let x = ratio;
+		let x = ratio.clone();
 
 		let num = x.numer();
 		let den = x.denom();
@@ -39,7 +40,7 @@ impl<F: FieldExt, const NUM_LIMBS: usize, const POWER_OF_TEN: usize>
 		let composed_den_f = compose_big_decimal_f::<F, NUM_LIMBS, POWER_OF_TEN>(den_decomposed);
 		let composed_den_f_inv = composed_den_f.invert().unwrap();
 		let res_f = composed_num_f * composed_den_f_inv;
-		assert!(res_f == score);
+		assert!(res_f == *score);
 
 		// Take the highest POWER_OF_TEN digits for comparison
 		// This just means lower precision
@@ -48,14 +49,18 @@ impl<F: FieldExt, const NUM_LIMBS: usize, const POWER_OF_TEN: usize>
 		let comp = first_limb_den * threshold;
 		let is_bigger = first_limb_num >= comp;
 
-		ThresholdWitness { threshold, is_bigger, num_decomposed, den_decomposed }
+		ThresholdWitness { threshold: threshold.clone(), is_bigger, num_decomposed, den_decomposed }
 	}
 }
 
 /// Witness structure for proving threshold checks
 pub struct ThresholdWitness<F: FieldExt, const NUM_LIMBS: usize> {
+	/// Threshold value to be checked with
 	pub threshold: F,
+	/// Comparison result
 	pub is_bigger: bool,
+	/// Target value numerator decomposition
 	pub num_decomposed: [F; NUM_LIMBS],
+	/// Target value denominator decomposition
 	pub den_decomposed: [F; NUM_LIMBS],
 }
