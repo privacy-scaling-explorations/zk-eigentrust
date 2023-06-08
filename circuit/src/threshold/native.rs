4089451d2fbaff45165be250f558dcdc87ca8dd1
lazovicff <lazovicff@gmail.com>
2023-06-08 12:30:38 +0200

    Updated tests

--- circuit/src/threshold/native.rs
+++ circuit/src/threshold/native.rs
@@ -31,6 +31,8 @@ impl<const NUM_LIMBS: usize, const POWER_OF_TEN: usize> Threshold<NUM_LIMBS, POW
 			decompose_big_decimal::<Fr, NUM_LIMBS, POWER_OF_TEN>(num.to_biguint().unwrap());
 		let den_decomposed =
 			decompose_big_decimal::<Fr, NUM_LIMBS, POWER_OF_TEN>(den.to_biguint().unwrap());
+		println!("{:?}", num_decomposed);
+		println!("{:?}", den_decomposed);
 
 		// Constraint checks - circuits should implement from this point
 		let composed_num_f = compose_big_decimal_f::<Fr, NUM_LIMBS, POWER_OF_TEN>(num_decomposed);
@@ -45,6 +47,9 @@ impl<const NUM_LIMBS: usize, const POWER_OF_TEN: usize> Threshold<NUM_LIMBS, POW
 		let first_limb_num = *num_decomposed.last().unwrap();
 		let first_limb_den = *den_decomposed.last().unwrap();
 		let comp = first_limb_den * threshold;
+		println!("comp: {:?}", comp);
+		println!("first_limb_num: {:?}", first_limb_num);
+		println!("threshold: {:?}", threshold);
 		let is_bigger = first_limb_num >= comp;
 
 		ThresholdWitness { threshold, is_bigger, num_decomposed, den_decomposed }
@@ -75,46 +80,84 @@ mod tests {
 	#[test]
 	fn test_check_threshold_1() {
 		const NUM_LIMBS: usize = 2;
-		const POWER_OF_TEN: usize = 2;
+		const POWER_OF_TEN: usize = 3;
 
-		let score_u128 = 356;
-		let score_sum_u128 = 1000;
-		let threshold_u128 = 500;
+		let threshold = 346;
+		let num = 345111;
+		let den = 1000;
 
-		let score = Fr::from_u128(score_u128) * Fr::from_u128(score_sum_u128).invert().unwrap();
-		let ratio = BigRational::new(
-			BigInt::from_u128(score_u128).unwrap(),
-			BigInt::from_u128(score_sum_u128).unwrap(),
-		);
-		let threshold = Fr::from_u128(threshold_u128);
-		let t: Threshold<NUM_LIMBS, POWER_OF_TEN> = Threshold::new(score, ratio, threshold);
+		let comp_u128 = num >= den * threshold;
+		println!("comp_u128: {:?}", comp_u128);
+
+		let num_bn = BigInt::from_u128(num).unwrap();
+		let den_bn = BigInt::from_u128(den).unwrap();
+
+		let threshold_fr = Fr::from_u128(threshold);
+		let num_fr = Fr::from_u128(num);
+		let den_fr = Fr::from_u128(den);
+
+		let score = num_fr * den_fr.invert().unwrap();
+
+		let ratio = BigRational::new(num_bn, den_bn);
+		let t: Threshold<NUM_LIMBS, POWER_OF_TEN> = Threshold::new(score, ratio, threshold_fr);
 		let tw = t.check_threshold();
 
-		let expected_comp = score_u128 > threshold_u128;
+		assert!(!tw.is_bigger);
+	}
 
-		assert!(tw.is_bigger == expected_comp);
+	#[test]
+	fn test_check_threshold_1_failing() {
+		const NUM_LIMBS: usize = 2;
+		const POWER_OF_TEN: usize = 3;
+
+		let threshold = 346;
+		let num = 345000;
+		let den = 1000;
+
+		let comp_u128 = num >= den * threshold;
+		println!("comp_u128: {:?}", comp_u128);
+
+		let num_bn = BigInt::from_u128(num).unwrap();
+		let den_bn = BigInt::from_u128(den).unwrap();
+
+		let threshold_fr = Fr::from_u128(threshold);
+		let num_fr = Fr::from_u128(num);
+		let den_fr = Fr::from_u128(den);
+
+		let score = num_fr * den_fr.invert().unwrap();
+
+		let ratio = BigRational::new(num_bn, den_bn);
+		let t: Threshold<NUM_LIMBS, POWER_OF_TEN> = Threshold::new(score, ratio, threshold_fr);
+		let tw = t.check_threshold();
+
+		assert!(!tw.is_bigger);
 	}
 
 	#[test]
 	fn test_check_threshold_2() {
 		const NUM_LIMBS: usize = 2;
-		const POWER_OF_TEN: usize = 4;
+		const POWER_OF_TEN: usize = 3;
 
-		let score_u128 = 56375;
-		let score_sum_u128 = 100000;
-		let threshold_u128: u128 = 50000;
+		let threshold = 344;
+		let num = 345111;
+		let den = 1000;
 
-		let score = Fr::from_u128(score_u128) * Fr::from_u128(score_sum_u128).invert().unwrap();
-		let ratio = BigRational::new(
-			BigInt::from_u128(score_u128).unwrap(),
-			BigInt::from_u128(score_sum_u128).unwrap(),
-		);
-		let threshold = Fr::from_u128(threshold_u128);
-		let t: Threshold<NUM_LIMBS, POWER_OF_TEN> = Threshold::new(score, ratio, threshold);
+		let comp_u128 = num >= den * threshold;
+		println!("comp_u128: {:?}", comp_u128);
+
+		let num_bn = BigInt::from_u128(num).unwrap();
+		let den_bn = BigInt::from_u128(den).unwrap();
+
+		let threshold_fr = Fr::from_u128(threshold);
+		let num_fr = Fr::from_u128(num);
+		let den_fr = Fr::from_u128(den);
+
+		let score = num_fr * den_fr.invert().unwrap();
+
+		let ratio = BigRational::new(num_bn, den_bn);
+		let t: Threshold<NUM_LIMBS, POWER_OF_TEN> = Threshold::new(score, ratio, threshold_fr);
 		let tw = t.check_threshold();
 
-		let expected_comp = score_u128 > threshold_u128;
-
-		assert!(tw.is_bigger == expected_comp);
+		assert!(tw.is_bigger);
 	}
 }
