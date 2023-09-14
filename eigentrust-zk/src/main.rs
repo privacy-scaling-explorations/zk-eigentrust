6d103c9b0a98e63ef1bb585aa85d12f3dde37d7d
lazovicff <lazovicff@gmail.com>
2023-09-14 15:53:45 +0200

    Params generation timing

--- eigentrust-zk/src/main.rs
+++ eigentrust-zk/src/main.rs
@@ -1,8 +1,13 @@
+use std::time::Instant;
+
 use eigentrust_zk::utils::{generate_params, write_params};
 use halo2::halo2curves::bn256::Bn256;
 
 fn main() {
 	let k = 20;
+	let start = Instant::now();
 	let params = generate_params::<Bn256>(k);
+	let end = start.elapsed();
+	println!("Params generation time: {:?}", end);
 	write_params(&params);
 }
